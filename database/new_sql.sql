015d0e238567c997c28b91a2ea90333be6094c99
Christy Francis <christy23francis@gmail.com>
2025-03-09 14:46:06 +0530

    Initial

---
+++ database/new_sql.sql
@@ -0,0 +1,2 @@
+INSERT INTO `wallet` (`wlt_id`, `Log_Id`, `cntno`, `name`, `age`, `sex`, `addr`, `email`, `waltid`, `amt`, `date`, `photo`) VALUES
+(2, 'BDRV689656212565', '8281227923', 'Christy', '24', 'Male', 'Kottayil', 'christy@gmail.com', '510503', 3000, '2025-02-17', 'cf_header_1701859513836_avatar.png');
